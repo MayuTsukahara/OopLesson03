f062eb782ec893fc64fc861af0f5b5b29ba19b51
MayuTsukahara <31091@ojs.ac.jp>
2020-09-25 15:57:26 +0900

    09252

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -11,24 +11,51 @@ namespace Chapter3 {
         public delegate bool Judgement(int value);
         static void Main(string[] args) {
 
-            var names = new List<string> {
-                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kong",
+            var numbers = new List<int> {
+                12,87,94,14,53,20,40,35,76,91,31,17,48
             };
 
+            //★3.1 練習問題
+
+            
+            var query = numbers.Exists(s=> s%8 == 0 || s%9 == 0);
+            Console.WriteLine(query);
+            Console.WriteLine();
+            
+
+
+            numbers.ForEach(s=> Console.WriteLine(s/2.0));
+            
+            Console.WriteLine();
+            
+
+            var query3 = numbers.Where(s=> s>=50);
+            foreach (var item in query3){
+                Console.WriteLine(item);
+	        }Console.WriteLine();
+
+
+            var query4 = numbers.Select(s=> s*2);
+            foreach (var item in query4){
+
+	        }
+
+            //query2.ForEach(s=> Console.WriteLine(s));
+	        
 
             //list.ForEach(s=> Console.WriteLine(s));
             //names.ConvertAll(s=>s.ToUpper()).ForEach(s => Console.WriteLine(s));
 
-            var query = names.Where(s=> s.Length <=5).ToList();
-            foreach (var s in query){
-                Console.WriteLine(s);
-	        }
-            Console.WriteLine("--------------");
+            //var query = names.Where(s=> s.Length <=5).ToList();
+            //foreach (var s in query){
+            //    Console.WriteLine(s);
+	        //}
+            //Console.WriteLine("--------------");
 
-            names[0] = "Osaka";
-            foreach (var item in query){
-                Console.WriteLine(item);
-	        }
+            //names[0] = "Osaka";
+            //foreach (var item in query){
+            //    Console.WriteLine(item);
+	        //}
 
             //var name = list.FindAll(s => s.Length <= 5);
 
