6e1a4560d7048172631a44bbe9cded9a21bd90a4
MayuTsukahara <31091@ojs.ac.jp>
2020-10-01 11:56:52 +0900

    演習4-.1

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -8,24 +8,28 @@ using System.Threading.Tasks;
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(GetProduct());
+            YearMonth ym = new YearMonth(2011,3);
+            if (ym.Is21Century)
+                Console.WriteLine("２１世紀ではない");
+            else
+                Console.WriteLine("２１世紀である");
+
+            Console.WriteLine("１ヶ月後は"+ym.AddOneMonth());
         }
         private static string GetProduct() {
             Sale sale = new Sale {
                 ShopName = "pet store",
-                Amount = 100000,
                 Product = "food"
             };
             sale = null;    //saleがnullだとnullが返ってくる
             return sale?.Product;   //nullじゃないからProductの値が返ってくる
         }
     }
-
     class Sale {
         //店舗名
         public string ShopName { get; set; }
         //売上高
-        public int Amount { get; set; }
+        public int Amount { get; set; } = 1000000;
         public　string Product { get; set; }
     }
 }
