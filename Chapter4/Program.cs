e2a101f98020b32c4b53e1334fa155490854bbca
MayuTsukahara <31091@ojs.ac.jp>
2020-10-02 13:44:53 +0900

    演習4.2

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -2,19 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            YearMonth ym = new YearMonth(2011,3);
-            if (ym.Is21Century)
-                Console.WriteLine("２１世紀ではない");
-            else
-                Console.WriteLine("２１世紀である");
+            var ymArray = new YearMonth[] {
+                new YearMonth(1996,3),
+                new YearMonth(2001,1),
+                new YearMonth(1999,5),
+                new YearMonth(2011,9),
+                new YearMonth(2019,4)
+            };
 
-            Console.WriteLine("１ヶ月後は"+ym.AddOneMonth());
+            foreach (var ym in ymArray) {
+                Console.WriteLine(ym.ToString());
+            }
+            Console.WriteLine();
+
+            if (ymArray == null)
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(ymArray);
+            Console.WriteLine();
+
+            var ymArrayPlus = ymArray.Select(ym=> ym.AddOneMonth()).OrderBy(ym=> ym.Month).OrderBy(ym=> ym.Year);
+            foreach (var ym in ymArrayPlus) {
+                Console.WriteLine(ym.ToString());
+            }
+
+
+
+
+
+
+
+
+        }
+        static YearMonth ReyearMonth(YearMonth[] ym) {
+            foreach (var y in ym) {
+                if(y.Is21Century)
+                    return y;
+            }
+            return null;
+            
         }
         private static string GetProduct() {
             Sale sale = new Sale {
