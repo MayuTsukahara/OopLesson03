fd26ba8f47a01080a8d8080b5d078a8d7442efcb
MayuTsukahara <31091@ojs.ac.jp>
2020-10-15 11:51:48 +0900

    1015

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -9,14 +9,56 @@ using System.Threading.Tasks;
 namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
-            //var lines = File.ReadAllLines("sample.txt");
-            //var we = new WordsExtractor(lines);
-            //foreach (var word in we.Extract()) {
-            //    Console.WriteLine(word);
-            //}
-            
+            Console.WriteLine("**********************");
+            Console.WriteLine("* 辞書登録プログラム *");
+            Console.WriteLine("**********************");
+            Console.WriteLine();
 
-            DuplicateKeySample();
+            var dict = new Dictionary<string, List<string>>();
+
+            while (true ) {
+                Console.WriteLine("1．登録　2．内容を表示　3.終了");
+                Console.Write(">");
+                var sentaku = int.Parse(Console.ReadLine());   
+                Console.WriteLine();
+
+                switch (sentaku) {
+                    case 1:
+                    
+                        Console.Write("KEYを入力：");
+                        var key = Console.ReadLine();
+                        Console.Write("VALUEを入力：");
+                        var value = Console.ReadLine();
+
+                        if (dict.ContainsKey(key)) {
+                            dict[key].Add(value);
+                        } else {
+                            dict[key] = new List<string> { value };
+                        }
+
+                        Console.WriteLine("登録しました！");
+                        Console.WriteLine();
+                
+                        break;
+
+                    case 2:
+                        foreach (var item in dict) {
+                            foreach (var term in item.Value) {
+                                Console.WriteLine("{0} : {1}", item.Key, term);
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 3:   
+                        break;
+                }
+
+                if (sentaku == 3) {
+                    break;
+                }
+            }
+            
         }
         static public void DuplicateKeySample() {
             // ディクショナリの初期化
