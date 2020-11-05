ef6f541f850b01c8b5ada6f243df1c7219a5af6d
MayuTsukahara <31091@ojs.ac.jp>
2020-11-05 11:20:12 +0900

    設定クラス作成

---
+++ SendMailApp/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SendMailApp {
+    class Configuration {
+        public string Smtp { get; set; } //SMTPサーバ
+        public string MailAddress { get; set; } //自メールアドレス（送信元）
+        public string PassWord { get; set; }    //パスワード
+        public int Port { get; set; }    //ポート番号
+        public bool Ssl { get; set; }   //SSL設定
+
+        //初期設定用
+        public void DefaultSet() {
+            Smtp = "smtp.gmail.com";
+            MailAddress = "ojsinfosys01@gmail.com";
+            PassWord = "ojsinfosys2020";
+            Port = 587;
+            Ssl = true;
+        }
+
+    }
+}
